f4bbc5d939486f7484f20f543f6c8642f0b41a8d
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-03-12 13:38:27 -0300

    Color palette

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#9C37EB',
+      'dark-purple': '#47196B',
+      'light-purple': '#BE7EEE',
+      'bold-purple': '#55396B',
+      'bright-purple': '#7A2AB8',
+      'white': '#FFFFFF',
+      'black': '#000000'
+    },
   },
   plugins: [],
 }
