0bc371a43f92bf6333308122b881967ad3cd647b
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-04-28 01:43:23 -0300

    Card Register Page

--- src/Components/FormInput.tsx
+++ src/Components/FormInput.tsx
@@ -1,11 +1,15 @@
 interface FormInputInterface {
     label?: string;
     type: string,
+    consultPackage: {
+        getValue: (name: string) => string;
+        setValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    },
     name: string,
     id: string,
 };
 
-const FormInput : React.FC<FormInputInterface> = ({ label, type, name, id }) => {
+const FormInput : React.FC<FormInputInterface> = ({ label, type, consultPackage, name, id }) => {
     return (
         <div className="md:flex items-center">
             {
@@ -15,6 +19,8 @@ const FormInput : React.FC<FormInputInterface> = ({ label, type, name, id }) => 
             }
             <input
                 type={ type }
+                value={ consultPackage.getValue(name) }
+                onChange={ consultPackage.setValue }
                 name={ name }
                 id={ id }
                 className='w-full mt-2 mb-5 md:my-4 py-1 px-5 border-2 border-dark-purple rounded-full'
