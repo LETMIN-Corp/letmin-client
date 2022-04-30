19850aeab20753d5a485c5ba5442e1fbfc08b98e
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-04-30 16:29:12 -0300

    Form formatting changed

--- src/Components/Inputs/FormInput.tsx
+++ src/Components/Inputs/FormInput.tsx
@@ -1,6 +1,7 @@
 interface ComponentInterface {
-    label?: string;
     type: string,
+    placeholder?: string
+    size?: 'small' | 'medium' | 'large',
     consultPackage: {
         getValue: (name: string) => string;
         setValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -9,23 +10,28 @@ interface ComponentInterface {
     id: string,
 };
 
-const FormInput : React.FC<ComponentInterface> = ({ label, type, consultPackage, name, id }) => {
+const FormInput : React.FC<ComponentInterface> = ({ type, placeholder, size, consultPackage, name, id }) => {
+    const getInputSize = () => {
+        switch(size) {
+            case 'small':
+                return 'md:w-3/12';
+            case 'medium':
+                return 'md:w-5/12';
+            case 'large':
+                return 'md:w-6/12'
+        }
+    }
+
     return (
-        <div className='items-center'>
-            {
-                label && (
-                    <label htmlFor={ id } className='mr-3 w-52'>{ label }</label>
-                )
-            }
-            <input
-                type={ type }
-                value={ consultPackage.getValue(name) }
-                onChange={ consultPackage.setValue }
-                name={ name }
-                id={ id }
-                className='w-full mt-2 mb-5 md:mt-2 py-1 px-5 border-2 border-dark-purple rounded-lg'
-            />
-        </div>
+        <input
+            type={ type }
+            placeholder={ placeholder }
+            value={ consultPackage.getValue(name) }
+            onChange={ consultPackage.setValue }
+            name={ name }
+            id={ id }
+            className={`${ getInputSize() } w-full md:text-lg mt-2 mb-5 md:mt-2 py-3 px-5 border-2 border-dark-purple rounded-lg`}
+        />
     );
 }
 
