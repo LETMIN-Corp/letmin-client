b789407de8ee45fc63a3be017cc2dc1fb02c00e8
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-06-08 13:19:15 -0300

    User default

---
+++ src/Views/User/UserProfle.tsx
@@ -0,0 +1,11 @@
+import UserDefault from "./UserDefault"
+
+const UserProfile = () => {
+    return (
+        <UserDefault>
+            
+        </UserDefault>
+    );
+}
+
+export default UserProfile;
