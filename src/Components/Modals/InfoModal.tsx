83a567310e462342e37c3614be300a2cad69fb07
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-06-20 14:07:19 -0300

    Update InfoModal.tsx

--- src/Components/Modals/InfoModal.tsx
+++ src/Components/Modals/InfoModal.tsx
@@ -13,7 +13,7 @@ const InfoModal:React.FC<ComponentInterface> = ({ title, children, handleClose})
                     <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
                         <div className='sm:flex sm:items-start'>
                             <div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 sm:mx-0 sm:h-10 sm:w-10'>
-                                <i className='fa-solid fa-circle-question text-4xl'></i>
+                                <i className="fa-solid fa-info text-4xl"></i>
                             </div>
                             <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
                                 <h3 className='text-lg leading-6 font-medium' id='modal-title'>{ title }</h3>
@@ -24,7 +24,7 @@ const InfoModal:React.FC<ComponentInterface> = ({ title, children, handleClose})
                         </div>
                     </div>
                     <div className='px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
-                        <button type='button' onClick={ handleClose } className='mt-3 w-full inline-flex justify-center rounded-md border shadow-sm px-4 py-2 bg-white text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'>Cancel</button>
+                        <button type='button' onClick={ handleClose } className='mt-3 w-full inline-flex justify-center rounded-md border shadow-sm px-4 py-2 bg-white text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'>Fechar</button>
                     </div>
                 </div>
             </div>
