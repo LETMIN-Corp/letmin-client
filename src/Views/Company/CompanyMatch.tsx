6b7a6c3002d14f1a265965f733660bf633469edd
Bianca <85531325+biancacmr@users.noreply.github.com>
2022-07-28 09:33:29 -0300

    Update CompanyMatch.tsx

--- src/Views/Company/CompanyMatch.tsx
+++ src/Views/Company/CompanyMatch.tsx
@@ -8,24 +8,23 @@ const CompanyMatch : React.FC = () => {
 
     return (
         <CompanyDefault>
-            <div className='p-5 h-screen fixed'>
+            <div className='p-5 w-full h-screen'>
                 <h1 className='text-2xl'>
                     <i className="fa-solid fa-user-check mr-2"></i>
                     <span>Combinações</span>
                 </h1>
-                <div className='flex h-2/3 justify-around mt-5'>
-                    <div className='flex items-center w-1/3'>
-                        <div className='rounded-xl bg-gray px-3 py-4 lg:h-80'>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus numquam, officiis ab culpa accusamus consectetur ad voluptatum, voluptates odio vero! Accusamus deleniti molestiae possimus quas et error ducimus architecto.</p>
+                <div className='lg:flex h-2/3 justify-around mt-5'>
+                    <div className='lg:flex lg:items-center w-full lg:w-1/3'>
+                        <div className='rounded-xl bg-gray px-3 py-4 h-80 w-full'>
                         </div>
                     </div>
-                    <div className='flex items-center'>
+                    <div className='lg:flex lg:items-center mt-10 lg:mt-0'>
                         <div>
-                            <div className='text-center mb-20'>
-                                <h2 className='text-5xl mb-4'>Combinação</h2>
-                                <h2 className='text-5xl'>Perfeita!</h2>
+                            <div className='text-center mb-10 lg:mb-20 '>
+                                <h2 className='lg:text-5xl md:text-4xl text-3xl mb-4'>Combinação</h2>
+                                <h2 className='lg:text-5xl md:text-4xl text-3xl'>Perfeita!</h2>
                             </div>
-                            <div className='flex items-center justify-between'>
+                            <div className='flex items-center justify-evenly lg:justify-between'>
                                 <div>
                                     <button className='bg-primary py-2 px-4 w-30 h-10 rounded-full text-white hover:bg-dark-purple ease-out duration-200'>
                                     <span>Ver perfil</span>
