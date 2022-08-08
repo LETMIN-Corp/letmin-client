b2d781ff990aa9f5b248134929fdcd05a1ada46e
Bia-Souza15 <besouza208@gmail.com>
2022-08-08 07:36:15 -0300

    indicators page in progress
    Co-Authored-By: Gabriel Gato <70043907+Gato-Gabriel@users.noreply.github.com>
    Co-Authored-By: cajuuuuu <85528914+cajuuuuu@users.noreply.github.com>

--- src/Views/Company/CompanyIndicators.tsx
+++ src/Views/Company/CompanyIndicators.tsx
@@ -1,10 +1,34 @@
 import CompanyDefault from './CompanyDefault';
+import VacancyData from '../../Components/Items/VacancyActions';
 import { useEffect } from 'react';
 
 const CompanyIndicators = () => {
     useEffect((): void => {
         window.document.title = 'Letmin - Indicadores';
     }, []);
+
+    let lista = ([
+        {
+            name: 'Pasta X',
+            candidate: 15,
+        },
+        {
+            name: 'Pasta Y',
+            candidate: 15,
+        },
+        {
+            name: 'Pasta Z',
+            candidate: 15,
+        },
+        {
+            name: 'Pasta W',
+            candidate: 15,
+        },
+        {
+            name: 'Pasta T',
+            candidate: 15,
+        },
+    ]);
 
     return (
         <CompanyDefault>
@@ -13,16 +37,51 @@ const CompanyIndicators = () => {
                     <i className="fa-solid fa-chart-line mr-2"></i>
                     <span>Indicadores</span>
                 </h1>
-                <div className='mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {/* <div className='mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
                     <div className='bg-primary w-full h-52 rounded-sm'></div>
-                    <div className='bg-primary w-full h-52 rounded-sm'></div>
-                    <div className='bg-primary w-full h-52 rounded-sm'></div>
+                </div> */}
+
+                <div className='bg-lilac w-full py-5 mt-5 rounded-sm drop-shadow-lg'>
+                    <div className='flex md:text-xl lg:text-2xl text-lg font-medium'> {/* Cabeçalho */}
+                        <div className='w-4/12 flex justify-center'>
+                            Vagas
+                        </div>
+                        <div className='w-4/12 flex justify-center'>
+                            Candidatos
+                        </div>
+                        <div className='w-4/12 flex justify-center'>
+                            Ações
+                        </div>
+                    </div>
+                    <div> {/* Corpo */}
+                        {/* <VacancyData name='Técnico de Informática' candidates={ 10 }/>
+                        <VacancyData name='Supervisor' candidates={ 15 }/>
+                        <VacancyData name='Gerente' candidates={ 8 }/>
+                        <VacancyData name='Mecânico' candidates={ 45 }/>
+                        <VacancyData name='Supervisor' candidates={ 15 }/>
+                        <VacancyData name='Gerente' candidates={ 8 }/>
+                        <VacancyData name='Mecânico' candidates={ 45 }/>
+                        <VacancyData name='Supervisor' candidates={ 15 }/>
+                        <VacancyData name='Gerente' candidates={ 8 }/>
+                        <VacancyData name='Mecânico' candidates={ 45 }/> */}
+
+                        {
+                            lista.length === 0 && (
+                                <div className='mt-5 text-center md:text-left text-dark-purple text-lg font-medium'>Nenhuma pasta encontrada</div>
+                            )
+                        }
+                        {
+                            lista.map((candidate) => <VacancyData name="name" candidates={ candidate } />)
+                        }
+
+                    </div>
                 </div>
+
             </div>
         </CompanyDefault>
     );
