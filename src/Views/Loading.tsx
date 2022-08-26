c02dbf7ecb0958ff000f82d240c7beaa35a5a231
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-08-26 08:39:27 -0300

    Revert "Update Loading.tsx"
    This reverts commit 2b68bfe1c9b463c2505bb71bf8625d7f77cc1a83.

--- src/Views/Loading.tsx
+++ src/Views/Loading.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from "react";
+import loading from '../Assets/loading.gif';
 
-const Cube : React.FC = () => {
-    return (
-        <div className='bg-primary w-5 h-5 mx-1 rounded-full'></div>
-    );
-}
-
-export const Loading : React.FC = () => {
-    const [percentage, setPercentage] = useState(0);
-    const [percentageArray, setPercentageArray] = useState([]);
-
-    useEffect(() => {
-        let counter = setTimeout(() => {
-            setPercentage(percentage + 1)
-        }, 500);
-        setPercentageArray([...percentageArray, percentage]);
-        if(percentage > 10) {
-            setPercentage(0);
-            setPercentageArray([]);
-            clearTimeout(counter);
-        }
-    }, [percentage]);
+export const Loading = () => {
 
     return (
-        <div className='bg-primary w-screen h-screen flex items-center justify-center'>
-            <div className='bg-white flex items-center justify-center rounded-md w-3/12 py-5'>
-                {
-                    percentageArray.map((key) => <Cube key={key} />)
-                }
-            </div>
+        <div>
+            <img src={ loading } alt="...loading" style={{ width: '200px', margin: ' 40px auto', display: 'block' }}/>
         </div>
     )
 };
-
-//  default Loading;
