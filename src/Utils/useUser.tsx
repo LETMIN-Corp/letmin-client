6234bb3537f45ff0e738862515f1f4ae36164e80
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-09-14 22:36:17 -0300

    User vacancy search loading

--- src/Utils/useUser.tsx
+++ src/Utils/useUser.tsx
@@ -2,9 +2,9 @@ import { useContext } from "react";
 import { AuthContext } from "../Contexts/AuthContextProvider";
 
 const useUser = () => {
-    const { getVacancies } : any = useContext(AuthContext);
+    const { getVacancies, loading } : any = useContext(AuthContext);
 
-    return { getVacancies };
+    return { getVacancies, loading };
 }
 
 export default useUser;    
--- src/Views/User/UserVacancySearch.tsx
+++ src/Views/User/UserVacancySearch.tsx
@@ -3,11 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import UserVacancySearchCard from '../../Components/Cards/UserVacancySearchCard';
 import List from '../../Components/Items/List';
+import Loading from '../../Components/Items/Loading';
 import useUser from '../../Utils/useUser';
 import UserDefault from './UserDefault'
 
 const UserVacancySearch = () => {
-    const User = useUser();
+    const user = useUser();
     const [allVacancies, setAllVacancies] = useState([]);
     const [vacancies, setVacancies] = useState([]);
     const [searchVacancies, setSearchVacancies] = useState('');
@@ -17,7 +18,7 @@ const UserVacancySearch = () => {
     }, []);
 
     useEffect(() => {
-        User.getVacancies().then((res : any) => {
+        user.getVacancies().then((res : any) => {
             setAllVacancies(res.data.vacancies);
             setVacancies(res.data.vacancies)
         })
@@ -54,7 +55,6 @@ const UserVacancySearch = () => {
                     <FontAwesomeIcon icon={ faBullhorn } className='mr-2' />
                     <span>Buscar Vagas</span>
                 </h1>
-
                 <div className='mt-5 flex flex-wrap'>
                     <div className='max-w-sm w-full relative'>
                         <input type='text' placeholder='Buscar' onChange={ (e) => setSearchVacancies(e.target.value) } className='w-full pl-2 pr-8 py-1 border-2 border-dark-purple rounded-md' name='search' id='search' />
@@ -62,24 +62,35 @@ const UserVacancySearch = () => {
                     </div>
                 </div>
                 {
-                    !!vacancyCards.length && (
-                        <div className='mt-7'>
-                            <p className='text-bright-gray font-bold text-sm md:text-md text-md mb-2'>Foram encontrados { vacancyCards.length } resultados</p>
-                        </div>
+                    user.loading && (
+                        <Loading />
                     )
                 }
-                <div className='grid grid-cols-1 flex flex-col justify-center items-center md:grid-cols-1 gap-7 w-full md:mb-5'>
-                    {
-                        !!vacancyCards.length && (                                
-                            <List data={ vacancyCards } itemsPerPage={ 10 }></List>
-                        )
-                    }
-                    {
-                        !vacancyCards.length && (
-                            <div className='mt-5 text-center md:text-left text-dark-purple text-lg font-medium'>Nenhum item encontrado</div>
-                        )
-                    }
-                </div>
+                {
+                    !user.loading && (
+                        <>      
+                            {
+                                !!vacancyCards.length && (
+                                    <div className='mt-7'>
+                                        <p className='text-bright-gray font-bold text-sm md:text-md text-md mb-2'>Foram encontrados { vacancyCards.length } resultados</p>
+                                    </div>
+                                )
+                            }
+                            <div className='grid grid-cols-1 flex flex-col justify-center items-center md:grid-cols-1 gap-7 w-full md:mb-5'>
+                                {
+                                    !!vacancyCards.length && (                                
+                                        <List data={ vacancyCards } itemsPerPage={ 10 }></List>
+                                    )
+                                }
+                                {
+                                    !vacancyCards.length && (
+                                        <div className='mt-5 text-center md:text-left text-dark-purple text-lg font-medium'>Nenhum item encontrado</div>
+                                    )
+                                }
+                            </div>
+                        </>
+                    )
+                }
             </div>
         </UserDefault>
     );
