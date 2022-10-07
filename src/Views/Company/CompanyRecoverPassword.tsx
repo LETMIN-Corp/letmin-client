9a367e011153a210d5776e61b227ddbb31f80137
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2022-10-06 23:14:11 -0300

    button with loading

--- src/Views/Company/CompanyRecoverPassword.tsx
+++ src/Views/Company/CompanyRecoverPassword.tsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
-import FormButton from "../../Components/Buttons/FormButton";
 import TextInput from "../../Components/Inputs/TextInput";
 import Footer from "../../Components/Layouts/Footer";
 import Menu from '../../Components/Layouts/Menu';
 import InputTypesEnum from "../../Enums/InputTypesEnum";
 import useAuth from "../../Utils/useAuth";
-import useLoading from "../../Utils/useLoading";
-import Loading from "../../Components/Items/Loading";
+import SubmitButton from "../../Components/Buttons/SubmitButton";
+import { Navigate, redirect, useNavigate } from "react-router-dom";
 
 const CompanyRecoverPassword : React.FC = () => {
     const auth = useAuth();
-    const { loading } = useLoading();
     const [emailSent, setEmailSent] = useState(false);
+    const navigate = useNavigate();
 
     useEffect((): void => {
         window.document.title = 'Letmin - Recuperar senha';
@@ -52,7 +51,6 @@ const CompanyRecoverPassword : React.FC = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        auth.setLoading(true);
         return await auth.sendRecoveryEmail(data.email).then((res: any) => {
             if (res.status !== 200) {
                 auth.dispatchError('Erro ao enviar email de recuperação');
@@ -60,6 +58,7 @@ const CompanyRecoverPassword : React.FC = () => {
             }
             auth.dispatchSuccess('Email de recuperação enviado com sucesso!');
             setEmailSent(true);
+            navigate('/company/login');
         });
     }
 
@@ -68,19 +67,9 @@ const CompanyRecoverPassword : React.FC = () => {
             <Menu menuButtons={ pageButtons } />
             <div className='w-screen min-h-screen flex flex-col items-center justify-center'>
                 <form onSubmit={ handleSubmit } className='w-full md:w-6/12 lg:w-3/12 p-5'>
-                    {
-                        loading && <Loading />
-                    }
-                    {
-                        emailSent ? <p className='text-green-500'>Email de recuperação enviado com sucesso!</p>    
-                        : (
-                            <>
-                                <h1 className='text-xl font-normal'>Recuperar senha</h1>
-                                <TextInput type={ InputTypesEnum.email } consultPackage={ consultPackage } placeholder='E-mail' name='email' />
-                                <FormButton text='Enviar' isFullWidth={ true } />
-                            </>
-                        )
-                    }
+                    <h1 className='text-xl font-normal'>Recuperar senha</h1>
+                    <TextInput type={ InputTypesEnum.email } consultPackage={ consultPackage } placeholder='E-mail' name='email' />
+                    <SubmitButton text='Enviar' loading={ auth.loading } />
                 </form>
             </div>
             <Footer />
